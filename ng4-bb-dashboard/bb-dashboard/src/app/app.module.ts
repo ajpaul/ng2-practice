9424280f886b49e1729b63bc64882c649570520b
Adam Paul <ajpaul89@gmail.com>
2017-08-31 10:47:26 -0400

    Checking in files. About to force myself to use command line branching operations instead of SourceTree

--- ng4-bb-dashboard/bb-dashboard/src/app/app.module.ts
+++ ng4-bb-dashboard/bb-dashboard/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TimeTileComponent } from './time-tile/time-tile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimeTileComponent
   ],
   imports: [
     BrowserModule
---
+++ ng4-bb-dashboard/bb-dashboard/src/app/time-tile/time-tile.component.ts
@@ -0,0 +1,11 @@
+import  { Component } from '@angular/core';
+
+@Component({
+    selector: 'time-tile',
+    templateUrl: './time-tile.component.html',
+    styleUrls: ['./time-tile.component.scss']
+})
+
+export class TimeTileComponent {
+    
+}
